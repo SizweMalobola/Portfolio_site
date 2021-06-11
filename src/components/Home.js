c61dc3552fc82a1782d5663e3b791a2ac7c2172c
SizweMalobola <25reyals@gmail.com>
2021-06-11 21:22:56 +0200

    added home intro with relevent styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,26 @@ const HOme = () => {
   const animationTimeline = new TimelineLite({ paused: true });
   return (
     <>
-      <div className={styles["container"]}></div>
+      <div className={styles["container"]}>
+        <section className={styles["home-top"]}>
+          <h1>web developer.</h1>
+          <div className={styles["intro-div"]}>
+            <div className={styles["row"]}>
+              <h3>VICTOR VON DOOM</h3>
+              <h1>Self taught web developer</h1>
+              <p>
+                It was late 2019 that I decided to pursue a career in the
+                software development industry and become a web developer. Since
+                then I've had my ups and downs, made sacrifices and I've
+                persevered. This is how badly I want to write code for a living.
+              </p>
+              <span>
+                <a href="/#">View Projects</a> or <a href="/#">Read About Me</a>
+              </span>
+            </div>
+          </div>
+        </section>
+      </div>
     </>
   );
 };
