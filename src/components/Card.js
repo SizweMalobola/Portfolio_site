0a8719c2c3fa678708d792bfd78193db427b0270
SizweMalobola <25reyals@gmail.com>
2021-06-12 21:23:20 +0200

    added props for height and more

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,20 @@
 import React from "react";
 import styles from "./cardStyle.module.css";
 
-function Card({ timeline, projectName, imgURL }) {
+function Card({ timeline, projectName, imgURL, height, more }) {
   return (
-    <a className={styles["container"]} href="/#">
+    <a
+      style={{ height: `${height}px` }}
+      className={styles["container"]}
+      href="/#"
+    >
       {/* add imgurl */}
       <div>
         <span>{timeline}</span>
         <h2>{projectName}</h2>
-        <span>View Case Study -{">"}</span>
+        <span>
+          {more} -{">"}
+        </span>
       </div>
     </a>
   );
