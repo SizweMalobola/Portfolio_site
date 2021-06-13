26a37e178bfda2502b1eb844023a3ebf0c86e322
SizweMalobola <25reyals@gmail.com>
2021-06-13 21:31:06 +0200

    setup state to control modal display

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./navbarStyle.module.css";
 import { TimelineLite } from "gsap/gsap-core";
 import { Link } from "react-router-dom";
+import ModalPortal from "./ModalPortal";
 
 function Navbar() {
+  const [showModal, setShowModal] = useState(false);
   const animationTimeline = new TimelineLite({ paused: true });
+  const clickHandler = () => {
+    showModal ? setShowModal(false) : setShowModal(true);
+  };
   return (
-    <nav className={styles["container"]}>
-      <Link to="/" className={styles["logo"]}>
-        <span>
-          <b>S</b>
-          <b>M</b>
-        </span>
-        <span>Web Developer</span>
-      </Link>
-      <div className={styles["menu"]}>
-        <span className={styles["menu-text"]}>MENU</span>
-        <b className={styles["menu-icons"]}>
-          <i></i>
-          <i></i>
-          <i></i>
-        </b>
-      </div>
-    </nav>
+    <>
+      <nav className={styles["container"]}>
+        <Link to="/" className={`${styles["logo"]}`}>
+          <span
+            style={
+              showModal
+                ? { color: "#8e8a8f", border: "4px solid #8e8a8f" }
+                : null
+            }
+          >
+            <b>S</b>
+            <b>M</b>
+          </span>
+          <span style={showModal ? { color: "#8e8a8f" } : null}>
+            Web Developer
+          </span>
+        </Link>
+        <div
+          onClick={clickHandler}
+          style={showModal ? { color: "#8e8a8f" } : null}
+          className={styles["menu"]}
+        >
+          <span className={styles["menu-text"]}>MENU</span>
+          {/* change color of bars if showModal is true */}
+          <b className={styles["menu-icons"]}>
+            <i></i>
+            <i></i>
+            <i></i>
+          </b>
+        </div>
+      </nav>
+      {showModal ? <ModalPortal /> : null}
+    </>
   );
 }
 
