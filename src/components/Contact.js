a0a38ddd24c815f8f7fdfad514e87a265597d46a
SizweMalobola <25reyals@gmail.com>
2021-06-13 15:46:55 +0200

    added Contact route & style module

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./contactStyle.module.css";
+
+function Contact() {
+  return (
+    <div>
+      <h1>hit me up and lets talk</h1>
+    </div>
+  );
+}
+
+export default Contact;
