cb673415bbcb80e8152b456b1097c966d8cea064
SizweMalobola <25reyals@gmail.com>
2021-06-13 15:44:50 +0200

    finished setting up routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Container from "./components/Container";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <Navbar />
-        <Home />
-      </Container>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Container>
+          <Navbar />
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/projects/:id" component={Projects} />
+        </Container>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
