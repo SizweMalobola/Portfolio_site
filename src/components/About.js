7ec3e5f4a713e1e46ec2bbc1f2dca753f07f2865
SizweMalobola <25reyals@gmail.com>
2021-06-13 15:46:34 +0200

    added About route & style module

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./aboutStyle.module.css";
+
+function About() {
+  return (
+    <div>
+      <h1>THIS IS THE ABOUT PAGE</h1>
+    </div>
+  );
+}
+
+export default About;
