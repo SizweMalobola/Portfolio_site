617b633766a4bcd3b650d7e1c3ec9482f52f9801
SizweMalobola <25reyals@gmail.com>
2021-06-14 21:38:08 +0200

    turned Footer into a functional component

---
+++ src/components/Footer.js
@@ -0,0 +1,75 @@
+import React from "react";
+import styles from "./footerStyle.module.css";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <footer>
+      <div className={styles["footer-top"]}>
+        <div className={styles["contact-info"]}>
+          <h3>
+            Contact <br /> information --
+          </h3>
+          <p>
+            Feel free to reach out to me any time. I prefer to talk over email,
+            especially since we may be a few time zones away.
+          </p>
+          <div>
+            <p>
+              E: <span>scjmalobola@gmail.com</span>
+            </p>
+            <p>
+              P: <span>+27 3823 8398</span>
+            </p>
+          </div>
+        </div>
+        <div className={styles["latest-project"]}>
+          <h3>
+            Latest
+            <br />
+            projects -
+          </h3>
+          <a href="/#">Iceland</a>
+          <a href="/#">Basic UX</a>
+          <a href="/#">Triniso</a>
+          <a href="/#">Symova</a>
+          <a href="/#">PMR</a>
+          <a href="/#">All projects</a>
+        </div>
+        <div className={styles["current-availability"]}>
+          <h3>
+            Current
+            <br />
+            availability -
+          </h3>
+          <p>
+            I usually work on several projects but I’ll be happy to discuss new
+            opportunities. Let’s get in touch!
+          </p>
+        </div>
+        <div className={styles["social-media"]}>
+          <h3>
+            Follow
+            <br />
+            me on -
+          </h3>
+        </div>
+      </div>
+      <div className={styles["footer-bottom"]}>
+        <Link to="/" className={styles["logo"]}>
+          <span>
+            <b>S</b>
+            <b>M</b>
+          </span>
+          <span>Web Developer</span>
+        </Link>
+        <div>
+          <span>&copy; 2021 Sizwe Malobola.</span>
+          <span>Go forth be conquered.</span>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
