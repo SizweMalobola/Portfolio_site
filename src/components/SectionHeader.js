dd6ab64ccc34a08b324b563d4f673fdd149451d4
SizweMalobola <25reyals@gmail.com>
2021-06-16 00:53:50 +0200

    created a style module for SectionHeader

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -1,27 +1,11 @@
 import React from "react";
+import styles from "./sectionHeaderStyle.module.css";
 
 function SectionHeader({ subtitle, title }) {
   return (
     <div>
-      <h3
-        style={{
-          fontSize: "1.1rem",
-          fontWeight: 350,
-          marginBottom: "1.6rem",
-          color: "#8e8a8f",
-        }}
-      >
-        {subtitle.toUpperCase()}
-      </h3>
-      <h1
-        style={{
-          fontSize: "5rem",
-          lineHeight: "4.8rem",
-          marginBottom: "1.6rem",
-        }}
-      >
-        {title}
-      </h1>
+      <h3 className={styles["subtitle"]}>{subtitle.toUpperCase()}</h3>
+      <h1 className={styles["title"]}>{title}</h1>
     </div>
   );
 }
