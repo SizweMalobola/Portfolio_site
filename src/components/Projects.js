a1b43c96b322695c228655469efe342f33076939
SizweMalobola <25reyals@gmail.com>
2021-06-15 16:40:33 +0200

    will work on Projects last

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,17 @@
 import React from "react";
 import styles from "./projectsStyle.module.css";
+
 function Projects({ match }) {
   return (
-    <div>
-      <h1>this is projects path {match.params.id}</h1>
+    <div className={styles["container"]}>
+      <div className={styles["project-details"]}>
+        <div className={styles["left"]}>
+          <h3>CASE STUDY</h3>
+          {/* <h2>{projectTitle}</h2> */}
+        </div>
+        <div className={styles["right"]}></div>
+      </div>
+      <div className={styles["images-grid"]}></div>
     </div>
   );
 }
