4a34b2df7b8b1c63d9104af9809e2320595d7294
SizweMalobola <25reyals@gmail.com>
2021-06-15 20:55:38 +0200

    removed navbar from Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,36 +20,32 @@ function Navbar() {
   }, [showModal]);
   return (
     <>
-      <nav className={styles["container"]}>
-        <Link to="/" className={`${styles["logo"]}`}>
-          <span
-            style={
-              showModal
-                ? { color: "#8e8a8f", border: "4px solid #8e8a8f" }
-                : null
-            }
-          >
-            <b>S</b>
-            <b>M</b>
-          </span>
-          <span style={showModal ? { color: "#8e8a8f" } : null}>
-            Web Developer
-          </span>
-        </Link>
-        <div
-          onClick={clickHandler}
-          style={showModal ? { color: "#8e8a8f" } : null}
-          className={styles["menu"]}
+      <Link to="/" className={`${styles["logo"]}`}>
+        <span
+          style={
+            showModal ? { color: "#8e8a8f", border: "4px solid #8e8a8f" } : null
+          }
         >
-          <span className={styles["menu-text"]}>MENU</span>
-          {/* change color of bars if showModal is true */}
-          <b className={styles["menu-icons"]}>
-            <i></i>
-            <i></i>
-            <i></i>
-          </b>
-        </div>
-      </nav>
+          <b>S</b>
+          <b>M</b>
+        </span>
+        <span style={showModal ? { color: "#8e8a8f" } : null}>
+          Web Developer
+        </span>
+      </Link>
+      <div
+        onClick={clickHandler}
+        style={showModal ? { color: "#8e8a8f" } : null}
+        className={styles["menu"]}
+      >
+        <span className={styles["menu-text"]}>MENU</span>
+        {/* change color of bars if showModal is true */}
+        <b className={styles["menu-icons"]}>
+          <i></i>
+          <i></i>
+          <i></i>
+        </b>
+      </div>
       {showModal ? <ModalPortal closeModal={clickHandler} /> : null}
     </>
   );
