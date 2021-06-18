cfb93b3c844e78ebb18622601f345e0103828900
SizweMalobola <25reyals@gmail.com>
2021-06-18 23:23:38 +0200

    added icons & moved elements 2%

--- src/components/FixedBottom.js
+++ src/components/FixedBottom.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./fixedBottomStyle.module.css";
+import { FaArrowUp, FaEnvelope } from "react-icons/fa";
 
 function FixedBottom() {
   // Todo put id of top to route for scroll up functionality to work
   return (
     <>
       <Link to="/contact" className={styles["envelope"]}>
-        M <span>contact me</span>
+        <FaEnvelope /> <span>contact me</span>
       </Link>
       {/* this will stay hidden until the page has reached a certain point */}
       <a className={styles["scroll-up"]} href="#top">
-        U
+        <FaArrowUp />
       </a>
     </>
   );
