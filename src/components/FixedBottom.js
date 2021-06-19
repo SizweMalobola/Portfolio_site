c36a2013fa40a504351fb2a3661bc60b781f67ad
SizweMalobola <25reyals@gmail.com>
2021-06-19 15:27:58 +0200

    scroll-up appears  according to scrollTop position

--- src/components/FixedBottom.js
+++ src/components/FixedBottom.js
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./fixedBottomStyle.module.css";
 import { FaArrowUp, FaEnvelope } from "react-icons/fa";
 
 function FixedBottom() {
-  // Todo put id of top to route for scroll up functionality to work
+  const [scrollPos, setScrollPos] = useState(null);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScrollPos(document.documentElement.scrollTop);
+    });
+  });
   return (
     <>
       <Link to="/contact" className={styles["envelope"]}>
         <FaEnvelope /> <span>contact me</span>
       </Link>
       {/* this will stay hidden until the page has reached a certain point */}
-      <button
-        onClick={() => {
-          window.scrollTo(0, 0);
-        }}
-        className={styles["scroll-up"]}
-      >
-        <FaArrowUp />
-      </button>
+      {scrollPos > 500 ? (
+        <button
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+          className={styles["scroll-up"]}
+        >
+          <FaArrowUp />
+        </button>
+      ) : null}
     </>
   );
 }
