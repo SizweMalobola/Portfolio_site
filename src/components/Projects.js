0b675c45795c67ba03f548b9a580b08a08708cc6
SizweMalobola <25reyals@gmail.com>
2021-06-22 21:35:45 +0200

    used fetch api to get & display json file data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,61 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GoToTop from "./GoToTop";
 import styles from "./projectsStyle.module.css";
 
 function Projects({ match }) {
+  const [data, setData] = useState();
+  const getData = () => {
+    fetch("/data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        setData(data[match.params.id - 1]);
+        console.log(data);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <div className={styles["container"]}>
         <div className={styles["project"]}>
           <div className={styles["project-overview"]}>
             <h3 className={styles["subtitle"]}>CASE STUDY</h3>
-            <h2 className={styles["title"]}>
-              Basic UX — design resources for InVision Studio
-            </h2>
+            <h2 className={styles["title"]}>{data && data.title}</h2>
             <h3 className={styles["subtitle"]}>TECH STACK USED</h3>
             <ul className={styles["list"]}>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
+              {data &&
+                data["tech_stack"].map((val, index) => {
+                  return <li key={index + 222}>{val}</li>;
+                })}
             </ul>
-            <a className={styles["link"]} href="/#">
+            <a className={styles["link"]} href={data && data["code_url"]}>
               View Code
             </a>
-            <a className={styles["link"]} href="/#">
+            <a className={styles["link"]} href={data && data["preview_url"]}>
               Live Preview
             </a>
           </div>
           <div className={styles["project-details"]}>
-            <p>
-              Studio is a new powerful tool where you can design, prototype and
-              animate your projects easily. One of InVision's initiatives is the
-              Design Forward Fund, a $5 million fund dedicated to investing in
-              the future of design. I'm happy to say that my idea was among
-              several selected for implementation and shortly after the initial
-              interviews, we began shaping requirements for the Basic UX
-              project.
-            </p>
-            <p>
-              Our goal was to create an extensive library of components and
-              assets to help designers wireframe their web projects faster. Over
-              the course of 5 months I designed more than 400 layout components
-              in 30 categories (each prepared for desktop, tablet & phone
-              screens), 128 icons, 56 user journey map elements and over 100
-              user interface elements. You can easily customize and combine them
-              when working on your wireframes.
-            </p>
-            <p>
-              Aside from that, I was in touch with the Director of Product and
-              the InVision tech team to report issues and bugs that I found
-              within the software, as well as to propose improvements regarding
-              certain functionalities. It was exciting to see how the product
-              evolved and improved with each update during this short time.
-            </p>
+            {data &&
+              data["paragraph_array"].map((val, index) => {
+                return <p key={index + 222}>{val}</p>;
+              })}
           </div>
         </div>
         <div className={styles["images-container"]}>
-          <div className={styles["image"]}></div>
-          <div className={styles["image"]}></div>
           <div className={styles["image"]}></div>
           <div className={styles["image"]}></div>
         </div>
