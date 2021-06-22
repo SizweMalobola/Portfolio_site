b858007ca876afe00dcbb6447817d265ff414a07
SizweMalobola <25reyals@gmail.com>
2021-06-22 08:12:28 +0200

    added Slideshow component & style module

---
+++ src/components/Slideshow.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./slideshowStyle.module.css";
+
+function Slideshow() {
+  return <div className={styles["container"]}></div>;
+}
+
+export default Slideshow;
