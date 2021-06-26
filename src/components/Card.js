c47923ac6432ad6eb47aac16442378c323d839f6
SizweMalobola <25reyals@gmail.com>
2021-06-26 21:08:38 +0200

    animated Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./cardStyle.module.css";
+import { gsap, ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Card({ timeline, projectName, imgURL, height, more, id }) {
+  let cardRef = null;
+  useEffect(() => {
+    gsap.from(cardRef, {
+      scrollTrigger: { trigger: cardRef },
+      duration: 0.8,
+      y: 120,
+      autoAlpha: 0,
+    });
+  }, [cardRef]);
+
   return (
     <Link
+      ref={(card) => (cardRef = card)}
       to={`/projects/${id}`}
       style={{ height: `${height}px` }}
       className={styles["container"]}
