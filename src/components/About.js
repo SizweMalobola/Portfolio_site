0d14750dfbb138bb8dfeafb5edfd551af4daecc2
SizweMalobola <25reyals@gmail.com>
2021-06-26 22:24:21 +0200

    animated About

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./aboutStyle.module.css";
 import Footer from "./Footer";
 import GoToTop from "./GoToTop";
 import Slideshow from "./Slideshow";
+import { gsap, ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function About() {
+  const introLinesRef = [];
+  const skillsRef = [];
+
+  useEffect(() => {
+    const tween = gsap.timeline();
+    tween.staggerFrom(
+      introLinesRef,
+      0.8,
+      { autoAlpha: 0, y: 20, ease: "power3.out" },
+      0.4
+    );
+    gsap.from(skillsRef[0], {
+      scrollTrigger: { trigger: skillsRef[0] },
+      duration: 0.8,
+      y: 100,
+      autoAlpha: 0,
+    });
+    gsap.from(skillsRef[1], {
+      scrollTrigger: { trigger: skillsRef[1] },
+      duration: 0.8,
+      y: 80,
+      autoAlpha: 0,
+    });
+  }, []);
   return (
     <>
       <div className={styles["container"]}>
-        <h3 className={styles["subtitle"]}>A LITTLE ABOUT ME</h3>
-        <h2 className={styles["title"]}>
+        <h3
+          ref={(line1) => {
+            introLinesRef.push(line1);
+          }}
+          className={styles["subtitle"]}
+        >
+          A LITTLE ABOUT ME
+        </h3>
+        <h2
+          ref={(line2) => {
+            introLinesRef.push(line2);
+          }}
+          className={styles["title"]}
+        >
           Yo, I’m Sizwe Malobola a self-taught, self-directed and
           self-discipline web developer. It’s surreal when I look back to when I
           started to learn web development. At the time I didn’t understand what
@@ -17,7 +56,12 @@ function About() {
           responsive fullstack website and put it on the internet for people to
           experience.
         </h2>
-        <p className={styles["content"]}>
+        <p
+          ref={(line3) => {
+            introLinesRef.push(line3);
+          }}
+          className={styles["content"]}
+        >
           I’m currently focusing on the frontend of web development. I’m also
           not a stranger to the backend, I’ve worked with Ruby & Ruby on Rails
           and I’m currently using Firebase a BAAS when I build fullstack
@@ -41,90 +85,78 @@ function About() {
         </p>
         <Slideshow />
         <section className={styles["skills"]}>
-          <div>
+          <div
+            ref={(div1) => {
+              skillsRef.push(div1);
+            }}
+          >
             <h2>Programming languages</h2>
             <div>
               <h3>HTML</h3>
-              <i>Proficient</i>
             </div>
             <div>
               <h3>CSS</h3>
-              <i>Proficient</i>
             </div>
             <div>
               <h3>SASS</h3>
-              <i>Working knowledge</i>
             </div>
             <div>
               <h3>JavaScript</h3>
-              <i>Proficient</i>
             </div>
             <div>
               <h3>Ruby</h3>
-              <i>Past experience</i>
             </div>
           </div>
-          <div>
+          <div
+            ref={(div2) => {
+              skillsRef.push(div2);
+            }}
+          >
             <h2>Frameworks, Libraries & Tools</h2>
             <div>
               <h3>ReactJS</h3>
-              <i>Proficient</i>
             </div>
             <div>
               <h3>Redux</h3>
-              <i>Working knowledge</i>
             </div>
             <div>
               <h3>Ruby On Rails</h3>
-              <i>Past experience</i>
             </div>
             <div>
               <h3>Bootstrap 4 & 5</h3>
-              <i>Working knowledge</i>
             </div>
             <div>
               <h3>MySQL</h3>
-              <i>Past experience</i>
             </div>
             <div>
               <h3>Git & GitHub</h3>
-              <i>Proficient</i>
             </div>
             <div>
               <h3>Jest</h3>
-              <i>Working knowledge</i>
             </div>
             <div>
               <h3>JQuery</h3>
-              <i>Past experience</i>
             </div>
             <div>
               <h3>Webpack</h3>
-              <i>Proficient</i>
             </div>
             <div>
               <h3>NPM</h3>
-              <i>Proficient</i>
             </div>
             <div>
               <h3>Heroku</h3>
-              <i>Past experience</i>
             </div>
             <div>
               <h3>Firebase</h3>
-              <i>Working knowledge</i>
             </div>
             <div>
               <h3>VS Code</h3>
-              <i>Proficient</i>
             </div>
             <div>
               <h3>Figma</h3>
-              <i>Past experience</i>
             </div>
             <div>
               <h3>Chrome Developer Tools</h3>
-              <i>Proficient</i>
             </div>
           </div>
         </section>
