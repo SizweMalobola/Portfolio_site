520e8457732918334447b245faf7d971b8052654
SizweMalobola <25reyals@gmail.com>
2021-06-26 20:54:45 +0200

    added SectionHeader

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./sectionHeaderStyle.module.css";
+import { gsap, ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function SectionHeader({ subtitle, title }) {
+  let divRef = null;
+  useEffect(() => {
+    gsap.from(divRef, {
+      scrollTrigger: { trigger: divRef },
+      duration: 0.8,
+      autoAlpha: 0,
+      y: 130,
+    });
+  }, [divRef]);
+
   return (
-    <div>
+    <div
+      ref={(div) => {
+        divRef = div;
+      }}
+    >
       <h3 className={styles["subtitle"]}>{subtitle.toUpperCase()}</h3>
       <h1 className={styles["title"]}>{title}</h1>
     </div>
